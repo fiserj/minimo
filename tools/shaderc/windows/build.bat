151d8935663cf2fbb584080913baed1f136c8590
Jakub Fiser <fiser87@gmail.com>
2021-04-08 10:44:39 +0100

    Fixed the Windows shaderc compilation script and added vswhere.exe.

--- tools/shaderc/windows/build.bat
+++ tools/shaderc/windows/build.bat
@@ -1,21 +1,44 @@
 @echo off
 setlocal
 
-set MSBUILD_EXE=
-for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set MSBUILD_EXE=%%D\MSBuild.exe
+for /F "delims=" %%T in ('vswhere -latest -format value -property catalog_productLineVersion') do set VS_VER=%%T
+for /F "delims=" %%T in ('vswhere -latest -format value -property installationPath') do set VS_PATH=%%T
 
-if not defined MSBUILD_EXE echo Error: Can't find MSBuild.exe. & goto :eof
-if not exist "%MSBUILD_EXE%" echo Error: %MSBUILD_EXE%: not found. & goto :eof
+if not defined VS_VER goto :no_vs_found
+if not defined VS_PATH goto :no_vs_found
+goto :vs_found
+
+:no_vs_found
+echo ERROR: Can't find any Visual Studio installation.
+goto :eof
+
+:vs_found
+echo Detected VS version and path: %VS_VER%, %VS_PATH%
+
+:: TODO : Not sure how far back this path format is stable.
+set MSBUILD_EXE="%VS_PATH%\MSBuild\Current\Bin\MSBuild.exe"
+
+if not exist %MSBUILD_EXE% (
+    echo ERROR: Can't locate MSBuild.exe at expected location: %MSBUILD_EXE%
+    goto :eof
+)
+
+:: https://developercommunity.visualstudio.com/t/windows-sdk-81-gone-from-vs2019/517851
+if "%VS_VER%" == "2019" (
+    set SDK_VER=10.0
+) else (
+    set SDK_VER=8.1
+)
+echo Setting wanted Windows SDK version as: %SDK_VER%
 
 set BGFX_DIR=..\..\..\third_party\bgfx
 pushd %BGFX_DIR%
 
-..\bx\tools\bin\windows\genie --with-tools vs2019
+..\bx\tools\bin\windows\genie --with-tools --with-windows=%SDK_VER% vs%VS_VER%
 
-:: TODO : Fix this.
-%MSBUILD_EXE% .build/projects/vs2019/shaderc.vcxproj /p:configuration=release /p:platform=x64
+%MSBUILD_EXE% .build/projects/vs%VS_VER%/shaderc.vcxproj /p:configuration=release /p:platform=x64
 
 popd
 
-copy /Y "%BGFX_DIR%\.build\win64_vs2019\bin\shadercRelease.exe" ".\shaderc.exe"
+copy /Y "%BGFX_DIR%\.build\win64_vs%VS_VER%\bin\shadercRelease.exe" ".\shaderc.exe"
 rd /S /Q "%BGFX_DIR%\.build"
