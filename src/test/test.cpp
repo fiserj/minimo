8d4ff8059b146a7ef31af51a7877ab9467144161
Jakub Fiser <fiser87@gmail.com>
2021-04-11 12:13:07 +0100

    Added testing cube scene.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,5 +1,60 @@
 #include <mnm/mnm.h>
 
+typedef struct
+{
+    float        x, y, z;
+    unsigned int color;
+
+} Vertex;
+
+static void quad(const Vertex* vertices, int i0, int i1, int i2, int i3)
+{
+    const Vertex* v0 = vertices + i0;
+    const Vertex* v1 = vertices + i1;
+    const Vertex* v2 = vertices + i2;
+    const Vertex* v3 = vertices + i3;
+
+    color (v0->color);
+    vertex(v0->x, v0->y, v0->z);
+
+    color (v1->color);
+    vertex(v1->x, v1->y, v1->z);
+
+    color (v2->color);
+    vertex(v2->x, v2->y, v2->z);
+
+    color (v0->color);
+    vertex(v0->x, v0->y, v0->z);
+
+    color (v2->color);
+    vertex(v2->x, v2->y, v2->z);
+
+    color (v3->color);
+    vertex(v3->x, v3->y, v3->z);
+}
+
+static void cube()
+{
+    static const Vertex vertices[] =
+    {
+        {  0.5f,  0.5f, -0.5f, 0xff00ffff }, // 0
+        { -0.5f,  0.5f, -0.5f, 0x0000ffff }, // 1
+        { -0.5f,  0.5f,  0.5f, 0x000000ff }, // 2
+        {  0.5f,  0.5f,  0.5f, 0xff0000ff }, // 3
+        {  0.5f, -0.5f,  0.5f, 0xffff00ff }, // 4
+        { -0.5f, -0.5f,  0.5f, 0x00ff00ff }, // 5
+        { -0.5f, -0.5f, -0.5f, 0x00ffffff }, // 6
+        {  0.5f, -0.5f, -0.5f, 0xffffffff }, // 7
+    };
+
+    quad(vertices, 0, 1, 2, 3);
+    quad(vertices, 4, 5, 6, 7);
+    quad(vertices, 3, 2, 5, 4);
+    quad(vertices, 7, 6, 1, 0);
+    quad(vertices, 2, 1, 6, 5);
+    quad(vertices, 0, 3, 4, 7);
+}
+
 static void setup(void)
 {
     size(800, 600, WINDOW_DEFAULT);
@@ -17,31 +72,26 @@ static void draw(void)
     projection();
     identity();
     perspective(60.0f, aspect(), 0.1f, 10.0f);
-    // ortho(-aspect(), aspect(), -1.0f, 1.0f, -10.0f, 10.0f);
+
+    static float y = 2.0f;
+    if (key_down(KEY_UP  )) { y += 0.5f; }
+    if (key_down(KEY_DOWN)) { y -= 0.5f; }
+    if (key_up  ('X'     )) { y  = 0.0f; }
 
     view();
     identity();
-    look_at(1.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-
-    static float x = 0.0f;
-    if (key_down(KEY_LEFT )) { x -= 0.25f; }
-    if (key_down(KEY_RIGHT)) { x += 0.25f; }
-    if (key_up  ('X'      )) { x  = 0.00f; }
+    look_at(2.0f, y, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
     model();
     identity();
-    translate(x, 0.0f, 0.0f);
+
+    static float angle = 0.0f;
+    angle += 0.5f;
+    rotate_y(angle);
 
     begin();
     {
-        color(0xff0000ff);
-        vertex(-0.6f, -0.4f, 0.0f);
-
-        color(0x00ff00ff);
-        vertex(0.6f, -0.4f, 0.0f);
-
-        color(0x0000ffff);
-        vertex(0.0f, 0.6f, 0.0f);
+        cube();
     }
     end();
 }
