54638efe17130ce35da8a4fd8fc238e9f24026af
Jakub Fiser <fiser@adobe.com>
2021-05-01 13:26:02 +0100

    Enabled BX debug configuration in Debug CMake configuration.

--- third_party/cmake/bx.cmake
+++ third_party/cmake/bx.cmake
@@ -11,6 +11,22 @@ target_include_directories(bx
         ${BX_DIR}/3rdparty
 )
 
+target_compile_definitions(bx PUBLIC
+    __STDC_CONSTANT_MACROS
+    __STDC_FORMAT_MACROS
+    __STDC_LIMIT_MACROS
+)
+
+target_compile_definitions(bx PRIVATE
+    "$<$<CONFIG:Debug>:BX_CONFIG_DEBUG=1>"
+)
+
+if(BX_CONFIG_DEBUG)
+    target_compile_definitions(bx PRIVATE
+        BX_CONFIG_DEBUG=1
+    )
+endif()
+
 if(APPLE)
     target_include_directories(bx PRIVATE
         ${BX_DIR}/include/compat/osx
@@ -19,8 +35,10 @@ elseif(WIN32)
     target_include_directories(bx PUBLIC
         ${BX_DIR}/include/compat/msvc
     )
+endif()
+
+if(MSVC)
     target_compile_definitions(bx PRIVATE
-        __STDC_FORMAT_MACROS
         _CRT_SECURE_NO_WARNINGS
     )
 endif()
