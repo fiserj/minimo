064c0d7c0a0f5919f62c3450156f073ca71671cb
Jakub Fiser <fiser87@gmail.com>
2021-05-08 23:07:34 +0100

    Added few example sketches.
    Since I'm already having second thoughts about some previous API decisions, I need to more concretely plan out the intended usage. Starting with three basic examples.

---
+++ src/test/static_geometry.cpp
@@ -0,0 +1,56 @@
+#include <mnm/mnm.h>
+
+#define CUBE_ID 1
+
+static void cube(void);
+
+static void scene(void);
+
+static void setup(void)
+{
+    title("Static Geometry Example");
+
+    begin(CUBE_ID, STATIC | COLOR);
+    cube();
+    end();
+}
+
+static void draw(void)
+{
+    projection();
+    identity();
+    perspective(60.0f, aspect(), 0.1f, 100.0f);
+
+    view();
+    identity();
+    look_at(0.0f, 0.0f, -17.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+    model();
+    identity();
+
+    scene();
+}
+
+void scene(void)
+{
+    for (float y = 0.0f; y < 11.0f; y += 1.0f)
+    for (float x = 0.0f; x < 11.0f; x += 1.0f)
+    {
+        push();
+
+        rotate_x (((float)elapsed() + x * 0.21f) * 57.2958f);
+        rotate_y (((float)elapsed() + y * 0.37f) * 57.2958f);
+        translate(-7.5f + x * 1.5f, -7.5f + y * 1.5f, 0.0f);
+
+        mesh(CUBE_ID);
+
+        pop();
+    }
+}
+
+void cube(void)
+{
+    // ...
+}
+
+MNM_MAIN(setup, draw, 0);
---
+++ src/test/transient_geometry.cpp
@@ -0,0 +1,56 @@
+#include <mnm/mnm.h>
+
+#define SCENE_ID 1
+
+static void cube(void);
+
+static void scene(void);
+
+static void setup(void)
+{
+    title("Transient Geometry Example");
+}
+
+static void draw(void)
+{
+    projection();
+    identity();
+    perspective(60.0f, aspect(), 0.1f, 100.0f);
+
+    view();
+    identity();
+    look_at(0.0f, 0.0f, -17.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+    model();
+    identity();
+
+    begin(SCENE_ID, COLOR);
+    scene();
+    end();
+
+    mesh(SCENE_ID);
+}
+
+void scene(void)
+{
+    for (float y = 0.0f; y < 11.0f; y += 1.0f)
+    for (float x = 0.0f; x < 11.0f; x += 1.0f)
+    {
+        push();
+
+        rotate_x (((float)elapsed() + x * 0.21f) * 57.2958f);
+        rotate_y (((float)elapsed() + y * 0.37f) * 57.2958f);
+        translate(-7.5f + x * 1.5f, -7.5f + y * 1.5f, 0.0f);
+
+        cube();
+
+        pop();
+    }
+}
+
+void cube(void)
+{
+    // ...
+}
+
+MNM_MAIN(setup, draw, 0);
