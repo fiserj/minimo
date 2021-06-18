f6110c653af6834d751ed16228672ebf784510b6
Jakub Fiser <fiser87@gmail.com>
2021-06-18 11:54:06 +0100

    Refactored built-in program loading.

--- src/lib/mnm_shaders.h
+++ src/lib/mnm_shaders.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <shaders/position_color_fs.h>    // position_color_fs
-#include <shaders/position_color_vs.h>    // position_color_vs
-#include <shaders/position_texcoord_fs.h> // position_texcoord_fs
-#include <shaders/position_texcoord_vs.h> // position_texcoord_vs
+#include <shaders/position_color_fs.h>          // position_color_fs
+#include <shaders/position_color_vs.h>          // position_color_vs
+#include <shaders/position_color_texcoord_fs.h> // position_color_texcoord_fs
+#include <shaders/position_color_texcoord_vs.h> // position_color_texcoord_vs
+#include <shaders/position_texcoord_fs.h>       // position_texcoord_fs
+#include <shaders/position_texcoord_vs.h>       // position_texcoord_vs
