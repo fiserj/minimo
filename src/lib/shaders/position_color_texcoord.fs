5bff70bf4b115641a284ca083f3af27c22f55e36
Jakub Fiser <fiser87@gmail.com>
2021-06-19 10:43:35 +0100

    Added support for per-vertex texcoords and single texture.

--- src/lib/shaders/position_color_texcoord.fs
+++ src/lib/shaders/position_color_texcoord.fs
@@ -1,10 +1,10 @@
 $input v_color0, v_texcoord0
 
-#include <bgfx_shader.sh>
+#include "common.sh"
 
 SAMPLER2D(s_tex_color, 0);
 
 void main()
 {
-    gl_FragColor = v_color0 * texture2D(s_tex_color, v_texcoord0);
+    gl_FragColor = v_color0 * texture2D(s_tex_color, FIX_TEXCOORD(v_texcoord0));
 }
--- src/lib/shaders/position_texcoord.fs
+++ src/lib/shaders/position_texcoord.fs
@@ -1,10 +1,10 @@
 $input v_texcoord0
 
-#include <bgfx_shader.sh>
+#include "common.sh"
 
 SAMPLER2D(s_tex_color, 0);
 
 void main()
 {
-    gl_FragColor = texture2D(s_tex_color, v_texcoord0);
+    gl_FragColor = texture2D(s_tex_color, FIX_TEXCOORD(v_texcoord0));
 }
