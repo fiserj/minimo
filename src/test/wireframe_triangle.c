833d4d78fa8b7c1ba342c85321f71b72bb6d3036
Jakub Fiser <fiser87@gmail.com>
2021-06-19 22:49:38 +0100

    Added more primitive types.

---
+++ src/test/wireframe_triangle.c
@@ -0,0 +1,40 @@
+#include <mnm/mnm.h>
+
+#define MESH_ID 1
+
+static void setup(void)
+{
+    title("Wireframe Cube Example");
+}
+
+static void draw(void)
+{
+    if (key_down(KEY_ESCAPE))
+    {
+        quit();
+    }
+
+    projection();
+    identity();
+    ortho(-aspect(), aspect(), -1.0f, 1.0f, 1.0f, -1.0f);
+
+    begin_transient(MESH_ID, PRIMITIVE_LINE_STRIP | VERTEX_COLOR);
+    {
+        color(0xff0000ff);
+        vertex(-0.6f, -0.4f, 0.0f);
+
+        color(0x00ff00ff);
+        vertex(0.6f, -0.4f, 0.0f);
+
+        color(0x0000ffff);
+        vertex(0.0f, 0.6f, 0.0f);
+
+        color(0xff0000ff);
+        vertex(-0.6f, -0.4f, 0.0f);
+    }
+    end();
+
+    mesh(MESH_ID);
+}
+
+MNM_MAIN(0, setup, draw, 0);
