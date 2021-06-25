fb64b2568d75f6cffae97c328eb5c6683416959f
Jakub Fiser <fiser87@gmail.com>
2021-06-25 09:36:16 +0100

    Adjusted the offscreen example proposal.
    It seems that it is going to be better to explicitly name the clear flags and pass framebuffer closer to the submitted geometry, to make the code more comprehensible.

--- src/test/offscreen.c
+++ src/test/offscreen.c
@@ -1,44 +1,36 @@
 #include <mnm/mnm.h>
 
-#define CUBE_ID       1
-#define QUAD_ID       2
+#define MESH_CUBE             1
+#define MESH_QUAD             2
 
-#define COLOR_TEX_ID  1
-#define DEPTH_TEX_ID  2
+#define TEXTURE_COLOR         1
+#define TEXTURE_DEPTH         2
 
-#define OFF_FBUF_ID   1
+#define FRAMEBUFFER_OFFSCREEN 1
 
-#define OFF_PASS_ID   1
+#define PASS_OFFSCREEN        1
 
 static void cube(void);
 
 static void offscreen_pass(void);
 
-static void main_pass(void);
+static void default_pass(void);
 
 static void setup(void)
 {
     title("Framebuffer Example");
 
-    clear_color(0x333333ff);
+    create_texture(TEXTURE_COLOR, TEXTURE_CLAMP | TEXTURE_TARGET               , SIZE_HALF, SIZE_HALF);
+    create_texture(TEXTURE_DEPTH, TEXTURE_CLAMP | TEXTURE_TARGET | TEXTURE_D32F, SIZE_HALF, SIZE_HALF);
 
-    create_texture(COLOR_TEX_ID, TEXTURE_CLAMP | TEXTURE_FB               , SIZE_HALF, SIZE_HALF);
-    create_texture(DEPTH_TEX_ID, TEXTURE_CLAMP | TEXTURE_FB | TEXTURE_D32F, SIZE_HALF, SIZE_HALF);
-
-    begin_framebuffer(OFF_FBUF_ID);
-    {
-        texture(COLOR_TEX_ID);
-        texture(DEPTH_TEX_ID);
-    }
+    begin_framebuffer(FRAMEBUFFER_OFFSCREEN);
+    texture(TEXTURE_COLOR);
+    texture(TEXTURE_DEPTH);
     end_framebuffer();
 
-    begin_pass(OFF_PASS_ID);
-    {
-        clear_color(0xccffccff);
-
-        framebuffer(OFF_FBUF_ID);
-    }
-    end_pass();
+    begin_static(MESH_CUBE, PRIMITIVE_QUADS | VERTEX_COLOR);
+    cube();
+    end();
 }
 
 static void draw(void)
@@ -48,15 +40,21 @@ static void draw(void)
         quit();
     }
 
-    begin_pass(OFF_PASS_ID);
-    offscreen_pass();
+    begin_pass(PASS_OFFSCREEN);
+    {
+        offscreen_pass();
+    }
     end_pass();
 
-    main_pass();
+    default_pass();
 }
 
-static void offscreen_pass(void)
+void offscreen_pass(void)
 {
+    clear_color(0xccffccff);
+
+    framebuffer(FRAMEBUFFER_OFFSCREEN);
+
     projection();
     identity();
     perspective(60.0f, aspect(), 0.1f, 100.0f);
@@ -68,19 +66,17 @@ static void offscreen_pass(void)
     model();
     identity();
 
-    begin_transient(CUBE_ID, PRIMITIVE_QUADS | VERTEX_COLOR);
-    {
-        rotate_x(((float)elapsed() + 1.0f) * 57.2958f);
-        rotate_y(((float)elapsed() + 2.0f) * 57.2958f);
-        cube();
-    }
-    end();
+    rotate_x(((float)elapsed() + 1.0f) * 57.2958f);
+    rotate_y(((float)elapsed() + 2.0f) * 57.2958f);
+    cube();
 
-    mesh(CUBE_ID);
+    mesh(MESH_CUBE);
 }
 
-static void main_pass(void)
+void default_pass(void)
 {
+    clear_color(0x333333ff);
+
     projection();
     ortho(-aspect(), aspect(), -1.0f, 1.0f, 1.0f, -1.0f);
 
@@ -90,7 +86,7 @@ static void main_pass(void)
     model();
     identity();
 
-    begin_transient(QUAD_ID, PRIMITIVE_QUADS | VERTEX_TEXCOORD);
+    begin_transient(MESH_QUAD, PRIMITIVE_QUADS | VERTEX_TEXCOORD);
     {
         const float a = aspect() * 0.5f;
 
@@ -108,10 +104,10 @@ static void main_pass(void)
     }
     end();
 
-    mesh(QUAD_ID);
+    mesh(MESH_QUAD);
 }
 
-static void cube(void)
+void cube(void)
 {
     color (0xfff200ff);
     vertex( 0.5f,  0.5f, -0.5f);
