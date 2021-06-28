64315d897e0cea00df15c8777300db914af2dde9
Jakub Fiser <fiser87@gmail.com>
2021-06-28 06:17:18 +0100

    Updated the API.

--- include/mnm/mnm.h
+++ include/mnm/mnm.h
@@ -4,55 +4,18 @@
 extern "C" {
 #endif
 
-// -----------------------------------------------------------------------------
-// MAIN ENTRY
-// -----------------------------------------------------------------------------
-
-/// Entry point when running as a library.
-///
-/// @param[in] init Function that runs once *before* the window and context are
-///   created.
-/// @param[in] setup Function that runs once *after* the window and context are
-///   created.
-/// @param[in] draw Function that runs on every frame.
-/// @param[in] cleanup Function that runs once just before the window is
-///   destroyed.
-///
-/// @returns Zero if no error occurred.
-///
-/// @attention Use `mnm::run` (see the end of this file), if compiling from C++
-///  to avoid possible warnings about exception safety (such as MSVC's C5039).
-///
-/// @warning This function must be called from the main thread only (the thread
-///   that calls `main`).
-///
-int mnm_run(void (* init)(void), void (* setup)(void), void (* draw)(void), void (* cleanup)(void));
-
 
 // -----------------------------------------------------------------------------
-// INIT
-// -----------------------------------------------------------------------------
-
-/// Sets the transient geometry per-thread memory budget. 32 MB by default. Must
-/// be called in the `init` callback (otherwise has no effect).
-///
-/// @param[in] megabytes Memory limit in MB.
-///
-void transient_memory(int megabytes);
-
-
-// -----------------------------------------------------------------------------
-// WINDOW
-// -----------------------------------------------------------------------------
+/// @section WINDOW
 
 /// Window creation flags.
 ///
 enum
 {
-    WINDOW_DEFAULT      = 0x00,
-    WINDOW_FIXED_SIZE   = 0x01,
-    WINDOW_FIXED_ASPECT = 0x02,
-    WINDOW_FULL_SCREEN  = 0x04,
+    WINDOW_DEFAULT      = 0x0000,
+    WINDOW_FIXED_SIZE   = 0x0001,
+    WINDOW_FIXED_ASPECT = 0x0002,
+    WINDOW_FULL_SCREEN  = 0x0004,
 };
 
 /// Changes window's size and attributes. The size is specified in "screen
@@ -66,6 +29,12 @@ enum
 /// @warning This function must be called from the main thread only.
 ///
 void size(int width, int height, int flags);
+
+/// Checks whether the window was resized.
+///
+/// @returns Non-zero if the window was resized.
+///
+int resized(void);
 
 /// Sets window title.
 ///
@@ -116,8 +85,7 @@ float dpi(void);
 
 
 // -----------------------------------------------------------------------------
-// INPUT
-// -----------------------------------------------------------------------------
+/// @section INPUT
 
 /// Special keys enum (alphabetical keys can be passed as characters).
 ///
@@ -212,8 +180,7 @@ int key_up(int key);
 
 
 // -----------------------------------------------------------------------------
-// TIME
-// -----------------------------------------------------------------------------
+/// @section TIME
 
 /// Returns time elapsed since the start of the application, in seconds. The
 /// timer is reset before the first `draw` call.
@@ -247,42 +214,42 @@ double toc(void);
 
 
 // -----------------------------------------------------------------------------
-// GEOMETRY
-// -----------------------------------------------------------------------------
+/// @section GEOMETRY
+///
+/// ...
 
-/// Mesh type. Static by default.
+/// Mesh flags.
 ///
 enum
 {
-    MESH_TRANSIENT = 0x40,
-    MESH_DYNAMIC   = 0x80,
-};
+    // Mesh type. Static by default.
+    MESH_TRANSIENT           = 0x0040,
 
-/// Vertex attribute flags. Position 3D always on.
-///
-enum
-{
-    VERTEX_COLOR    = 0x01,
-    VERTEX_NORMAL   = 0x02,
-    VERTEX_TEXCOORD = 0x04,
-};
+    // Vertex attribute flags. 3D position always on.
+    VERTEX_COLOR             = 0x0001,
+    VERTEX_NORMAL            = 0x0002,
+    VERTEX_TEXCOORD          = 0x0004, 
 
-/// Primitive type. Triangles by default.
-///
-enum
-{
-    PRIMITIVE_QUADS          = 0x08,
-    PRIMITIVE_TRIANGLE_STRIP = 0x10,
-    PRIMITIVE_LINES          = 0x18,
-    PRIMITIVE_LINE_STRIP     = 0x20,
+    // Primitive type. Triangles by default.
+    PRIMITIVE_QUADS          = 0x0008,
+    PRIMITIVE_TRIANGLE_STRIP = 0x0010,
+    PRIMITIVE_LINES          = 0x0018,
+    PRIMITIVE_LINE_STRIP     = 0x0020,
+    PRIMITIVE_POINTS         = 0x0028,
 };
 
 /// Starts mesh geometry recording. Mesh type, primitive type and attributes
-/// recorded per-vertex are specified via flags. Transient meshes are only valid
-/// in the current frame. The amount of transient geometry in a single frame is
-/// limited and can be specified in the init phase via `transient_memory` call.
+/// recorded per-vertex are specified via flags.
+///
+/// Transient meshes are only valid in the current frame. The amount of
+/// transient geometry in a single frame is limited and can be specified in the
+/// init phase via `transient_memory` call.
 ///
 void begin_mesh(int id, int flags);
+
+/// Ends the current geometry recording.
+///
+void end_mesh(void);
 
 /// Emits a vertex with given coordinates and current state (color, etc.). The
 /// vertex position is multiplied by the current model matrix.
@@ -315,10 +282,6 @@ void normal(float nx, float ny, float nz);
 ///
 void texcoord(float u, float v);
 
-/// Ends the current geometry recording.
-///
-void end_mesh(void);
-
 /// Submits recorded mesh geometry, using the same identifier.
 ///
 /// @param[in] id Mesh identifier.
@@ -327,33 +290,39 @@ void mesh(int id);
 
 
 // -----------------------------------------------------------------------------
-// TEXTURING
-// -----------------------------------------------------------------------------
+/// @section TEXTURING
+///
+/// Textures serve to add detail and also as framebuffer attachments for
+/// offscreen rendering.
 
-/// Texture attribute flags.
+/// Texture flags.
 ///
 enum
 {
     // Linear sampling, repeat border mode, RGBA8 format.
-    TEXTURE_DEFAULT = 0x00,
+    TEXTURE_DEFAULT    = 0x0000,
 
     // Sampling. Linear if no flag provided.
-    TEXTURE_NEAREST = 0x01,
+    TEXTURE_NEAREST    = 0x0001,
 
     // Border mode. Repeat if no flag provided.
-    TEXTURE_MIRROR  = 0x02,
-    TEXTURE_CLAMP   = 0x04,
+    TEXTURE_MIRROR     = 0x0002,
+    TEXTURE_CLAMP      = 0x0004,
 
     // Format. RGBA8 if no flag provided.
-    TEXTURE_R8      = 0x08,
-    TEXTURE_D24S8   = 0x10,
-    TEXTURE_D32F    = 0x18,
+    TEXTURE_R8         = 0x0008,
+    TEXTURE_D24S8      = 0x0010,
+    TEXTURE_D32F       = 0x0018,
 
     // Render target properties. Nothing if no flag provided.
-    TEXTURE_TARGET  = 0x40,
+    TEXTURE_TARGET     = 0x0040,
+    TEXTURE_READ_BACK  = 0x0080,
+    TEXTURE_WRITE_ONLY = 0x0100,
+    TEXTURE_BLIT_DST   = 0x0200,
 };
 
-/// Automatic texture size related to backbuffer size.
+/// Automatic texture size related to backbuffer size. When a window is resized,
+/// so is the texture.
 ///
 enum
 {
@@ -393,22 +362,29 @@ void texture(int id);
 
 
 // -----------------------------------------------------------------------------
-// PASSES
-// -----------------------------------------------------------------------------
-
-/// Delimits a section in which all `mesh` calls submit draw calls to the
-/// provided pass. Calls can be nested and the default pass is on implicitly.
-/// Passes directly correspond to BGFX's view concept, so they are primarily
-/// used for sorting draw calls into 
+/// @section PASSES
 ///
-void begin_pass(int id);
-
-/// Ends the current pass.
+/// Passes correspond to BGFX's views concept and are primarily used for
+/// sorting draw submissions. Each pass has several attached properties -
+/// viewport, view and projection transformations and optional clear state and
+/// framebuffer.
 ///
-void end_pass(void);
+/// Active passes are thread-local and "sticky" - a pass will be active until
+/// another call of `pass` function.
+///
+/// Submitting to a pass from multiple threads at once is safe, but setting its
+/// properties is not implicitly synchronized.
+
+/// Sets the active pass (thread-local). The pass stays active until next call
+/// of the function, and is persistent across multiple frame. By default, pass
+/// zero is active.
+///
+/// @param[in] id Pass identifier.
+///
+void pass(int id);
 
 /// Resets clear flags for the active pass (so that no clearing is done). By
-/// default both depth (1.0) and color (0x000000ff) are cleared.
+/// default nothing is cleared.
 ///
 void no_clear(void);
 
@@ -439,12 +415,20 @@ void viewport(int x, int y, int width, int height);
 
 
 // -----------------------------------------------------------------------------
-// FRAMEBUFFERS
-// -----------------------------------------------------------------------------
+/// @section FRAMEBUFFERS
+///
+/// Framebuffers serve as render targets for offscreen rendering. They are
+/// composed of individual texture attachments that must be created beforehand,
+/// and associated with the framebuffer in an begin/end block.
 
 /// Starts framebuffer building. Framebuffers are comprised of one or more
 /// texture attachments, which can be associated with them by calling `texture`
 /// inside the begin / end pair.
+///
+/// Any existing framebuffer associated with the used ID is destroyed, but not
+/// its textures.
+///
+/// @param[in] id Framebuffer identifier.
 ///
 void begin_framebuffer(int id);
 
@@ -454,26 +438,31 @@ void end_framebuffer(void);
 
 
 // -----------------------------------------------------------------------------
-// TRANSFORMATIONS
-// -----------------------------------------------------------------------------
-
-/// Sets model matrix stack as the active one.
+/// @section TRANSFORMATIONS
 ///
-void model(void);
+/// Each thread has an implicit matrix stack, that works similarly to how stack
+/// worked in the old OpenGL, but \a MiNiMo only has a single stack.
+///
+/// Each submitted mesh's model matrix is implicitly assigned by using the
+/// stack's top matrix when `mesh` function is called.
+///
+/// View and projection matrices of each pass are explicitly copied from the top
+/// matrix when `view` and `projection` functions are called. This explicitness
+/// is needed since multiple threads can submit draws to the same pass.
 
-/// Sets view matrix stack as the active one.
+/// Copies the curent matrix stack's top to the active pass' view matrix.
 ///
 void view(void);
 
-/// Sets projection matrix stack as the active one.
+/// Copies the curent matrix stack's top to the active pass' projection matrix.
 ///
 void projection(void);
 
-/// Pushes the top of the active matrix stack.
+/// Pushes the matrix stack down by one, duplicating the current matrix.
 ///
 void push(void);
 
-/// Pops the top of the active matrix stack.
+/// Pops the matrix stack, replacing the current matrix with the one below it.
 ///
 void pop(void);
 
@@ -566,8 +555,16 @@ void translate(float x, float y, float z);
 
 
 // -----------------------------------------------------------------------------
-// MULTITHREADING
-// -----------------------------------------------------------------------------
+/// @section MULTITHREADING
+///
+/// Multithreading in \a MiNiMo is very basic. At init time, a thread pool is
+/// created with `std::thread::hardware_concurrency` minus two threads (main
+/// one and BGFX rendering one), but always at least one. Individual task can
+/// then be submitted and they are queued and executed on the
+/// first-come-first-serve basis.
+///
+/// Resources can be created and draw primitives submitted from any thread, but
+/// synchronization infrastructure is provided at the moment.
 
 /// Adds an asynchronous task to the queue. Tasks can be created also from
 /// within other tasks.
@@ -584,8 +581,16 @@ int task(void (* func)(void* data), void* data);
 
 
 // -----------------------------------------------------------------------------
-// MISCELLANEOUS
-// -----------------------------------------------------------------------------
+/// @section MISCELLANEOUS
+///
+/// Miscellaneous utilities that don't belong in other sections. 
+
+/// Sets the transient geometry per-thread memory budget. 32 MB by default. Must
+/// be called in the `init` callback (otherwise has no effect).
+///
+/// @param[in] megabytes Memory limit in MB.
+///
+void transient_memory(int megabytes);
 
 /// Returns the current frame number, starting with zero-th frame.
 ///
@@ -595,24 +600,42 @@ int frame(void);
 
 
 // -----------------------------------------------------------------------------
+/// @section MAIN ENTRY
+///
+/// \a MiNiMo has a single blocking entry point and optional macro implementing
+/// C/C++ main entry is provided.
+
+/// Entry point when running as a library. Uses the provided callbacks and
+/// blocks until the app finishes.
+///
+/// @param[in] init Function that runs once *before* the window and context are
+///   created.
+/// @param[in] setup Function that runs once *after* the window and context are
+///   created.
+/// @param[in] draw Function that runs on every frame.
+/// @param[in] cleanup Function that runs once just before the window is
+///   destroyed.
+///
+/// @returns Zero if no error occurred.
+///
+/// @attention Use `mnm::run` (see the end of this file), if compiling from C++
+///  to avoid possible warnings about exception safety (such as MSVC's C5039).
+///
+/// @warning This function must be called from the main thread only (the thread
+///   that calls `main`).
+///
+int mnm_run(void (* init)(void), void (* setup)(void), void (* draw)(void), void (* cleanup)(void));
 
 #ifdef __cplusplus
-}
-#endif
-
-
-// -----------------------------------------------------------------------------
-// MAIN ENTRY (C++)
-// -----------------------------------------------------------------------------
-
-#ifdef __cplusplus
+} // extern "C" {
 
 #define MNM_MAIN_NAME mnm::run
 
 namespace mnm
 {
 
-/// Entry point when running as a library. C++ variant of `mnm_run` function. 
+/// C++ variant of `mnm_run` function. Provided as a courtesy to avoid possible
+/// warnings about exception safety.
 ///
 int run(void (* init)(void), void (* setup)(void), void (* draw)(void), void (* cleanup)(void));
 
@@ -621,11 +644,6 @@ int run(void (* init)(void), void (* setup)(void), void (* draw)(void), void (* 
 #else
 #   define MNM_MAIN_NAME mnm_run
 #endif // __cplusplus
-
-
-// -----------------------------------------------------------------------------
-// MAIN ENTRY IMPLEMENTATION
-// -----------------------------------------------------------------------------
 
 /// Helper macro to instantiate basic main function that just runs either
 /// `mnm_run` (if compiled as C file), or `mnm::run` (if compiled as C++).
@@ -637,5 +655,3 @@ int run(void (* init)(void), void (* setup)(void), void (* draw)(void), void (* 
         (void)argv; \
         return MNM_MAIN_NAME(init, setup, draw, cleanup); \
     }
-
-// -----------------------------------------------------------------------------
