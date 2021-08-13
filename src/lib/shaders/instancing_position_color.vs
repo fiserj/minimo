ba519ad7bc3735319cc3975bea4dde3500a75387
Jakub Fiser <fiser87@gmail.com>
2021-08-13 22:50:04 +0100

    Fixed the instancing.
    Still need to improve the example.

--- src/lib/shaders/instancing_position_color.vs
+++ src/lib/shaders/instancing_position_color.vs
@@ -6,6 +6,6 @@ $output v_color0
 void main()
 {
     mat4 model  = mtxFromCols(i_data0, i_data1, i_data2, i_data3);
-    gl_Position = mul(u_modelViewProj, mul(model, vec4(a_position, 1.0)));
+    gl_Position = mul(u_viewProj, mul(model, vec4(a_position, 1.0)));
     v_color0    = a_color0;
 }
--- src/test/instancing.c
+++ src/test/instancing.c
@@ -1,10 +1,12 @@
 #include <mnm/mnm.h>
 
-#define CUBE_MESH 1
+#define CUBE_MESH      1
+
+#define CUBE_INSTANCES 1
 
 static void cube(void);
 
-static void scene(void);
+// static void scene(void);
 
 static void setup(void)
 {
@@ -16,6 +18,14 @@ static void setup(void)
     begin_mesh(CUBE_MESH, PRIMITIVE_QUADS | VERTEX_COLOR);
     cube();
     end_mesh();
+
+    begin_instancing(CUBE_INSTANCES, INSTANCE_TRANSFORM);
+    {
+        identity();
+        translate(-7.5f * 1.5f, -7.5f, 0.0f);
+        instance(0);
+    }
+    end_instancing();
 }
 
 static void draw(void)
@@ -33,27 +43,29 @@ static void draw(void)
     look_at(0.0f, 0.0f, -17.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
     view();
 
-    identity();
+    // identity();
 
-    scene();
+    instances(CUBE_INSTANCES);
+    mesh(CUBE_MESH);
+    // scene();
 }
 
-void scene(void)
-{
-    for (float y = 0.0f; y < 11.0f; y += 1.0f)
-    for (float x = 0.0f; x < 11.0f; x += 1.0f)
-    {
-        push();
+// void scene(void)
+// {
+//     for (float y = 0.0f; y < 11.0f; y += 1.0f)
+//     for (float x = 0.0f; x < 11.0f; x += 1.0f)
+//     {
+//         push();
 
-        rotate_x (((float)elapsed() + x * 0.21f) * 57.2958f);
-        rotate_y (((float)elapsed() + y * 0.37f) * 57.2958f);
-        translate(-7.5f + x * 1.5f, -7.5f + y * 1.5f, 0.0f);
+//         rotate_x (((float)elapsed() + x * 0.21f) * 57.2958f);
+//         rotate_y (((float)elapsed() + y * 0.37f) * 57.2958f);
+//         translate(-7.5f + x * 1.5f, -7.5f + y * 1.5f, 0.0f);
 
-        mesh(CUBE_MESH);
+//         mesh(CUBE_MESH);
 
-        pop();
-    }
-}
+//         pop();
+//     }
+// }
 
 static void cube(void)
 {
