b51984429345ae3c39b58615417bb3bd29dba685
Jakub Fiser <fiser87@gmail.com>
2021-10-24 22:45:05 +0200

    Added basic text rendering example.

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -2,14 +2,28 @@
 
 #include <mnm/mnm.h>
 
+#include "editor_font.h"                   // g_editor_*
 #include "editor_ui.h"                     // Editor
+
+
+// -----------------------------------------------------------------------------
+// RESOURCE IDS
+// -----------------------------------------------------------------------------
+
+#define FONT_ID  127
+
+#define ATLAS_ID 1023
+
+#define TEXT_ID  4095
 
 
 // -----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 // -----------------------------------------------------------------------------
 
-// ...
+TextEdit         g_te;
+
+TextEditSettings g_tes;
 
 
 // -----------------------------------------------------------------------------
@@ -22,11 +36,15 @@ static void setup()
 
     clear_color(0x303030ff);
     clear_depth(1.0f);
-}
 
-static void cleanup()
-{
-    // ...
+    create_font(FONT_ID, g_font_data);
+
+    begin_atlas(ATLAS_ID, ATLAS_H_OVERSAMPLE_2X | ATLAS_ALLOW_UPDATE, FONT_ID, g_tes.font_cap_height * dpi());
+    glyph_range(0x20, 0x7e);
+    end_atlas();
+
+    // NOTE : Just a test content for now.
+    set_content(g_te, load_string("../src/test/static_geometry.c"));
 }
 
 static void update()
@@ -36,7 +54,18 @@ static void update()
         quit();
     }
 
-    // ...
+    begin_text(TEXT_ID, ATLAS_ID, TEXT_TRANSIENT);
+    identity();
+    submit_lines(g_te, g_tes, height());
+    end_text();
+
+    identity();
+    ortho(0.0f, width(), height(), 0.0f, 1.0f, -1.0f);
+    projection();
+
+    identity();
+    translate(10.0f, 15.0f, 0.0f);
+    mesh(TEXT_ID);
 }
 
 
@@ -44,4 +73,4 @@ static void update()
 // MINIMO MAIN ENTRY
 // -----------------------------------------------------------------------------
 
-MNM_MAIN(nullptr, setup, update, cleanup);
+MNM_MAIN(nullptr, setup, update, nullptr);
