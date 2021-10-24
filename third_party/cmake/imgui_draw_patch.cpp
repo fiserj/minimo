412c5e4af3af2696dc03b81963c6856e89b6736c
Jakub Fiser <fiser87@gmail.com>
2021-10-24 11:20:57 +0200

    Added better patch for proper cap height.

--- third_party/cmake/imgui_draw_patch.cpp
+++ third_party/cmake/imgui_draw_patch.cpp
@@ -1,11 +1,43 @@
 #include <imgui_draw.cpp>
 
-extern "C" unsigned int stb_decompress_exported
+extern "C" ImFont* ImGui_Patch_ImFontAtlas_AddFontFromMemoryCompressedTTF
 (
-    unsigned char*       output,
-    const unsigned char* i,
-    unsigned int         length
+    ImFontAtlas* atlas,
+    const void*  compressed_data,
+    unsigned int compressed_size,
+    float        cap_height
 )
 {
-    return stb_decompress(output, i, length);
+    const unsigned int decompressed_size = stb_decompress_length(reinterpret_cast<const unsigned char*>(compressed_data));
+    unsigned char*     decompressed_data = static_cast<unsigned char*>(IM_ALLOC(decompressed_size));
+
+    stb_decompress(
+        decompressed_data,
+        reinterpret_cast<const unsigned char*>(compressed_data),
+        compressed_size
+    );
+
+    stbtt_fontinfo info = {};
+    stbtt_InitFont(&info, decompressed_data, 0);
+
+    float pixel_height_for_cap_height = cap_height;
+
+    if (const int table = stbtt__find_table(info.data, info.fontstart, "OS/2"))
+    {
+        if (ttUSHORT(info.data + table) >= 2) // Version.
+        {
+            const float default_cap_height = ttSHORT(info.data + table + 88); // sCapHeight.
+
+            int ascent, descent;
+            stbtt_GetFontVMetrics(&info, &ascent, &descent, nullptr);
+
+            pixel_height_for_cap_height = (ascent - descent) * cap_height / default_cap_height;
+        }
+    }
+
+    return atlas->AddFontFromMemoryTTF(
+        decompressed_data,
+        decompressed_size,
+        pixel_height_for_cap_height
+    );
 }
