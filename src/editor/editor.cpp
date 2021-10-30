63264aa892510e63b2fac6c8344af09f8b7879c8
Jakub Fiser <fiser87@gmail.com>
2021-10-31 00:16:18 +0100

    Added initial stub of own GUI utilities.

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -1,9 +1,12 @@
-#include <assert.h>                        // assert
+#include <assert.h>      // assert
+#include <stdint.h>      // uint*_t
+
+#include <vector>        // vector
 
 #include <mnm/mnm.h>
 
-#include "editor_font.h"                   // g_editor_*
-#include "editor_ui.h"                     // Editor
+#include "editor_font.h" // g_editor_*
+#include "editor_ui.h"   // Editor
 
 
 // -----------------------------------------------------------------------------
@@ -15,6 +18,125 @@
 #define ATLAS_ID 1023
 
 #define TEXT_ID  4095
+
+#define LINES_ID 4094
+
+
+// -----------------------------------------------------------------------------
+// UTILITY MACROS
+// -----------------------------------------------------------------------------
+
+// TODO : Better assert.
+#define ASSERT(cond) assert(cond)
+
+
+// -----------------------------------------------------------------------------
+// TYPE ALIASES
+// -----------------------------------------------------------------------------
+
+template <typename T>
+using Vector = std::vector<T>;
+
+
+// -----------------------------------------------------------------------------
+// GUI HELPERS
+// -----------------------------------------------------------------------------
+
+enum struct State : uint8_t
+{
+    COLD,
+    HOT,
+    ACTIVE,
+};
+
+struct Rect
+{
+    float x0 = 0.0f;
+    float y0 = 0.0f;
+    float x1 = 0.0f;
+    float y1 = 0.0f;
+};
+
+class IdStack
+{
+public:
+    inline bool operator==(const IdStack& other) const
+    {
+        return m_hash == other.m_hash;
+    }
+
+    inline uint8_t size() const
+    {
+        return m_size;
+    }
+
+    inline uint8_t empty() const
+    {
+        return m_size == 0;
+    }
+
+    inline uint8_t top() const
+    {
+        ASSERT(m_size > 0);
+        return m_stack[m_size - 1];
+    }
+
+    inline void push(uint8_t id)
+    {
+        ASSERT(m_size < 7);
+        m_stack[m_size++] = id;
+    }
+
+    inline uint8_t pop()
+    {
+        ASSERT(m_size > 0);
+        return m_stack[--m_size];
+    }
+
+private:
+    union
+    {
+        struct
+        {
+            uint8_t  m_size;
+            uint8_t  m_stack[7];
+        };
+
+        uint64_t     m_hash = 0;
+    };
+};
+
+static IdStack g_active_stack;
+
+static IdStack g_current_stack;
+
+static bool mouse_over(const Rect& rect)
+{
+    const float x = mouse_x();
+    const float y = mouse_y();
+
+    return x >= rect.x0 && x < rect.x1 &&
+           y >= rect.y0 && y < rect.y1 ;
+}
+
+static bool button_logic(uint8_t id, const Rect& rect, bool enabled, State& out_state)
+{
+    out_state = State::COLD;
+
+    if (mouse_over(rect) && g_active_stack.empty())
+    {
+        out_state = State::HOT;
+
+        if (mouse_down(MOUSE_LEFT))
+        {
+            g_active_stack.push(id);
+            out_state = State::ACTIVE;
+        }
+    }
+
+    return
+        mouse_up(MOUSE_LEFT) && g_active_stack.top() == id && mouse_over(rect);
+}
 
 
 // -----------------------------------------------------------------------------
