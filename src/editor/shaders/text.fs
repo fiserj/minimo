c90c838bc473f0a2bea9b6cd0d2d4e45f3e01441
Jakub Fiser <fiser87@gmail.com>
2021-11-07 21:56:01 +0000

    Fixed the text shader.
    Stupid me.

--- src/editor/shaders/text.fs
+++ src/editor/shaders/text.fs
@@ -2,9 +2,10 @@ $input v_color0, v_texcoord0
 
 #include "../../lib/shaders/common.sh"
 
-SAMPLER2D(s_tex_color_rgba, 0);
+SAMPLER2D(s_tex_color_r, 0);
 
 void main()
 {
-    gl_FragColor = v_color0 * texture2D(s_tex_color_rgba, FIX_TEXCOORD(v_texcoord0));
+    float alpha  = texture2D(s_tex_color_r, FIX_TEXCOORD(v_texcoord0)).r;
+    gl_FragColor = vec4(v_color0.rgb, v_color0.a * alpha);
 }
