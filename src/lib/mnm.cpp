a64d00481fcc419173f6cbb68b6f4658f4b7e20d
Jakub Fiser <fiser87@gmail.com>
2022-02-18 12:46:38 +0000

    Slightly simplified `VertexLayoutCache`.

--- src/lib/mnm.cpp
+++ src/lib/mnm.cpp
@@ -262,22 +262,6 @@ static inline u16 mesh_type(u32 flags)
     return types[((flags & MESH_TYPE_MASK) >> MESH_TYPE_SHIFT)];
 }
 
-// NOTE : Currently unused.
-// static inline u16 mesh_primitive(u32 flags)
-// {
-//     constexpr u16 primitives[] =
-//     {
-//         PRIMITIVE_TRIANGLES,
-//         PRIMITIVE_QUADS,
-//         PRIMITIVE_TRIANGLE_STRIP,
-//         PRIMITIVE_LINES,
-//         PRIMITIVE_LINE_STRIP,
-//         PRIMITIVE_POINTS,
-//     };
-
-//     return primitives[((flags & PRIMITIVE_TYPE_MASK) >> PRIMITIVE_TYPE_SHIFT)];
-// }
-
 static inline u16 mesh_attribs(u32 flags)
 {
     return (flags & VERTEX_ATTRIB_MASK);
@@ -721,45 +705,41 @@ private:
 // VERTEX LAYOUT CACHE
 // -----------------------------------------------------------------------------
 
-// TODO : Add support for user defined layouts.
 class VertexLayoutCache
 {
-public:
-    VertexLayoutCache()
-    {
-        m_handles.fill(BGFX_INVALID_HANDLE);
-    }
+    // This silliness is just to be able to zero-initialize (and avoid having
+    // `StaticArray` support non-POD types).
+    using VertexLayoutMemory = StaticArray<uint8_t, sizeof(bgfx::VertexLayout)>;
 
-    // NOTE : Can't be done in constructor, as we need BGFX to be initialized.
+    StaticArray<      VertexLayoutMemory, 128> m_layouts;
+    StaticArray<bgfx::VertexLayoutHandle, 128> m_handles;
+
+public:
     void init()
     {
-        //  +-------------------------- VERTEX_COLOR
-        //  |  +----------------------- VERTEX_NORMAL
-        //  |  |  +-------------------- VERTEX_TEXCOORD
+        m_layouts.fill({});
+        m_handles.fill(BGFX_INVALID_HANDLE);
+
+        //  +-------------- VERTEX_COLOR
+        //  |  +----------- VERTEX_NORMAL
+        //  |  |  +-------- VERTEX_TEXCOORD
         //  |  |  |
-        add<0, 0, 0>();
-        add<1, 0, 0>();
-        add<0, 1, 0>();
-        add<0, 0, 1>();
-        add<1, 1, 0>();
-        add<1, 0, 1>();
-        add<0, 1, 1>();
-        add<1, 1, 1>();
+        variant<0, 0, 0>();
+        variant<1, 0, 0>();
+        variant<0, 1, 0>();
+        variant<0, 0, 1>();
+        variant<1, 1, 0>();
+        variant<1, 0, 1>();
+        variant<0, 1, 1>();
+        variant<1, 1, 1>();
     }
 
     inline void clear()
     {
-        for (bgfx::VertexLayoutHandle& handle : m_handles)
+        for (u32 i = 0; i < m_handles.size(); i++)
         {
-            destroy_if_valid(handle);
+            destroy_if_valid(m_handles[i]);
         }
-    }
-
-    inline void add(u32 flags)
-    {
-        constexpr u32 ATTRIB_MASK = VERTEX_ATTRIB_MASK | TEXCOORD_F32;
-        
-        add(flags & ATTRIB_MASK, 0);
     }
 
     bgfx::VertexLayoutHandle resolve_alias(u32& inout_flags, u32 alias_flags)
@@ -768,7 +748,7 @@ public:
         const u32 alias_attribs = mesh_attribs(alias_flags);
 
         const u32 skips = orig_attribs & (~alias_attribs);
-        const u32 idx   = get_index_from_flags(orig_attribs, skips);
+        const u32 idx   = index(orig_attribs, skips);
 
         inout_flags &= ~skips;
 
@@ -777,17 +757,17 @@ public:
 
     inline const bgfx::VertexLayout& operator[](u32 flags) const
     {
-        return m_layouts[get_index_from_flags(flags)];
+        return *reinterpret_cast<const bgfx::VertexLayout*>(&m_layouts[index(flags)]);
     }
 
 private:
-    static inline constexpr u32 get_index_from_flags(u32 attribs, u32 skips = 0)
+    static inline constexpr u32 index(u32 attribs, u32 skips = 0)
     {
         static_assert(
             VERTEX_ATTRIB_MASK >>  VERTEX_ATTRIB_SHIFT       == 0b0000111 &&
            (VERTEX_ATTRIB_MASK >> (VERTEX_ATTRIB_SHIFT - 3)) == 0b0111000 &&
             TEXCOORD_F32       >>  6                         == 0b1000000,
-            "Invalid index assumptions in `VertexLayoutCache::get_index_from_attribs`."
+            "Invalid index assumptions in `VertexLayoutCache::index`."
         );
 
         return
@@ -802,11 +782,11 @@ private:
         bool HasTexCoord,
         bool HasTexCoordF32 = false
     >
-    inline void add()
+    inline void variant()
     {
         if constexpr (HasTexCoord && !HasTexCoordF32)
         {
-            add<HasColor, HasNormal, HasTexCoord, true>();
+            variant<HasColor, HasNormal, HasTexCoord, true>();
         }
 
         constexpr u16 Flags =
@@ -815,72 +795,73 @@ private:
             (HasTexCoord    ? VERTEX_TEXCOORD : 0) |
             (HasTexCoordF32 ? TEXCOORD_F32    : 0) ;
 
-        add(Flags);
+        variant(Flags);
     }
 
-    void add(u32 attribs, u32 skips)
+    inline void variant(u32 flags)
+    {
+        constexpr u32 ATTRIB_MASK = VERTEX_ATTRIB_MASK | TEXCOORD_F32;
+        
+        variant(flags & ATTRIB_MASK, 0);
+    }
+
+    void variant(u32 attribs, u32 skips)
     {
         ASSERT(attribs == (attribs & (VERTEX_ATTRIB_MASK | TEXCOORD_F32)));
         ASSERT(skips == (skips & VERTEX_ATTRIB_MASK));
         ASSERT(skips != attribs || attribs == 0);
         ASSERT(skips == (skips & attribs));
 
+        bgfx::VertexLayout layout;
+        layout.begin();
+
+        if (attribs == VERTEX_POSITION)
         {
-            MutexScope lock(m_mutex);
+            layout.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float);
+        }
 
-            bgfx::VertexLayout layout;
-            layout.begin();
+        if (!!(skips & VERTEX_COLOR))
+        {
+            layout.skip(4 * sizeof(u8));
+        }
+        else if (!!(attribs & VERTEX_COLOR))
+        {
+            layout.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true);
+        }
 
-            if (attribs == VERTEX_POSITION)
+        if (!!(skips & VERTEX_NORMAL))
+        {
+            layout.skip(4 * sizeof(u8));
+        }
+        else if (!!(attribs & VERTEX_NORMAL))
+        {
+            layout.add(bgfx::Attrib::Normal, 4, bgfx::AttribType::Uint8, true, true);
+        }
+
+        if (!!(skips & VERTEX_TEXCOORD))
+        {
+            layout.skip(2 * (!!(attribs & TEXCOORD_F32) ? sizeof(f32) : sizeof(int16_t)));
+        }
+        else if (!!(attribs & VERTEX_TEXCOORD))
+        {
+            if (!!(attribs & TEXCOORD_F32))
             {
-                layout.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float);
+                layout.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float);
             }
-
-            if (!!(skips & VERTEX_COLOR))
+            else
             {
-                layout.skip(4 * sizeof(u8));
+                layout.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Int16, true, true);
             }
-            else if (!!(attribs & VERTEX_COLOR))
-            {
-                layout.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true);
-            }
+        }
 
-            if (!!(skips & VERTEX_NORMAL))
-            {
-                layout.skip(4 * sizeof(u8));
-            }
-            else if (!!(attribs & VERTEX_NORMAL))
-            {
-                layout.add(bgfx::Attrib::Normal, 4, bgfx::AttribType::Uint8, true, true);
-            }
+        layout.end();
+        ASSERT(layout.getStride() % 4 == 0);
 
-            if (!!(skips & VERTEX_TEXCOORD))
-            {
-                layout.skip(2 * (!!(attribs & TEXCOORD_F32) ? sizeof(f32) : sizeof(int16_t)));
-            }
-            else if (!!(attribs & VERTEX_TEXCOORD))
-            {
-                if (!!(attribs & TEXCOORD_F32))
-                {
-                    layout.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float);
-                }
-                else
-                {
-                    layout.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Int16, true, true);
-                }
-            }
+        const u16 idx = index(attribs, skips);
+        ASSERT(!bgfx::isValid(m_handles[idx]));
 
-            layout.end();
-            ASSERT(layout.getStride() % 4 == 0);
-
-            const u16 idx = get_index_from_flags(attribs, skips);
-
-            ASSERT(m_layouts[idx].getStride() == 0);
-            ASSERT(!bgfx::isValid(m_handles[idx]));
-
-            m_layouts[idx] = layout;
-            m_handles[idx] = bgfx::createVertexLayout(layout);
-        } // MutexScope
+        m_layouts[idx] = *reinterpret_cast<VertexLayoutMemory*>(&layout);
+        m_handles[idx] = bgfx::createVertexLayout(layout);
 
         // Add variants with skipped attributes (for aliasing).
         if (attribs && !skips)
@@ -889,16 +870,11 @@ private:
             {
                 if ((attribs & VERTEX_ATTRIB_MASK) != (skips & VERTEX_ATTRIB_MASK) && (attribs & skips) == skips)
                 {
-                    add(attribs, skips);
+                    variant(attribs, skips);
                 }
             }
         }
     }
-
-private:
-    Mutex                                m_mutex;
-    Array<bgfx::VertexLayout, 128>       m_layouts;
-    Array<bgfx::VertexLayoutHandle, 128> m_handles;
 };
 
 
