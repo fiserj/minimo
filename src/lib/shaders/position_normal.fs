5fa184743d030dbf1456b654d180798e582f8550
Jakub Fiser <fiser87@gmail.com>
2022-02-26 10:10:17 +0000

    Added normals' transformation.

--- src/lib/shaders/position_normal.fs
+++ src/lib/shaders/position_normal.fs
@@ -1,9 +1,9 @@
 $input v_normal
 
 #include <bgfx_shader.sh>
+#include <shaderlib.sh>
 
 void main()
 {
-    // NOTE : Normals are packed as RGB, so no visualization conversion needed.
-    gl_FragColor = vec4(v_normal, 1.0);
+    gl_FragColor = vec4(encodeNormalUint(v_normal), 1.0);
 }
