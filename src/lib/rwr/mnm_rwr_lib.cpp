4b083f0f21297c62ce6bfddf5d0095b5f3a56187
Jakub Fiser <fiser87@gmail.com>
2022-04-13 09:04:41 +0100

    Ported public cursor API.

--- src/lib/rwr/mnm_rwr_lib.cpp
+++ src/lib/rwr/mnm_rwr_lib.cpp
@@ -12,11 +12,13 @@ void size(int width, int height, int flags)
         t_ctx->is_main_thread,
         "`size` must be called from main thread only."
     );
+
     ASSERT(
         g_ctx->window_info.display_scale.X > 0.0f,
         "Invalid horizontal display scale (%.1f).",
         g_ctx->window_info.display_scale.X
     );
+
     ASSERT(
         g_ctx->window_info.display_scale.Y > 0.0f,
         "Invalid vertical display scale (%.1f).",
@@ -104,6 +106,46 @@ int pixel_width(void)
 int pixel_height(void)
 {
     return g_ctx->window_info.framebuffer_size.Y;
+}
+
+
+// -----------------------------------------------------------------------------
+/// PUBLIC API IMPLEMENTATION - CURSOR
+// -----------------------------------------------------------------------------
+
+void cursor(int type)
+{
+    ASSERT(
+        t_ctx->is_main_thread,
+        "`cursor` must be called from main thread only."
+    );
+
+    ASSERT(
+        type >= CURSOR_ARROW && type <= CURSOR_LOCKED,
+        "Invalid cursor type %i.",
+        type
+    );
+
+    if (g_ctx->active_cursor != u32(type))
+    {
+        g_ctx->active_cursor = u32(type);
+
+        switch (type)
+        {
+        case CURSOR_HIDDEN:
+            glfwSetInputMode(g_ctx->window_handle, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+            break;
+        case CURSOR_LOCKED:
+            glfwSetInputMode(g_ctx->window_handle, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            break;
+        default:
+            if (type >= CURSOR_ARROW && type <= CURSOR_LOCKED)
+            {
+                glfwSetInputMode(g_ctx->window_handle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+                glfwSetCursor   (g_ctx->window_handle, g_ctx->window_cursors[u32(type)]);
+            }
+        }
+    }
 }
 
 
