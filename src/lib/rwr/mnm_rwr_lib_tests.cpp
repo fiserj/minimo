29ffbacc0e70d54b563c723b29e3afc3548888cf
Jakub Fiser <fiser87@gmail.com>
2022-04-22 16:35:16 +0100

    Improved `StackAllocator` unit test.

--- src/lib/rwr/mnm_rwr_lib_tests.cpp
+++ src/lib/rwr/mnm_rwr_lib_tests.cpp
@@ -50,7 +50,7 @@ TEST_CASE("Arena Allocator")
 
     // Initialization.
     init(allocator, buffer.data, sizeof(buffer.data));
-    REQUIRE(allocator.size == sizeof(buffer));
+    REQUIRE(allocator.size == sizeof(buffer.data));
     REQUIRE(allocator.top == 0);
     REQUIRE(allocator.last == 0);
 
@@ -58,7 +58,7 @@ TEST_CASE("Arena Allocator")
     void* first = BX_ALLOC(&allocator, 13);
     REQUIRE(first != nullptr);
     REQUIRE(allocator.owns(first));
-    REQUIRE(allocator.size == sizeof(buffer));
+    REQUIRE(allocator.size == sizeof(buffer.data));
     REQUIRE(allocator.top == 13);
     REQUIRE(allocator.last == 0);
 
@@ -124,53 +124,67 @@ TEST_CASE("Arena Allocator")
 
 TEST_CASE("Stack Allocator")
 {
-    u64 buffer[16];
+    BX_ALIGN_DECL_16(struct) Buffer
+    {
+        u8 data[128] = {};
+    };
 
+    Buffer         buffer;
     StackAllocator allocator;
-    init(allocator, buffer, sizeof(buffer));
-    REQUIRE(allocator.size == sizeof(buffer));
+
+    // Initialization.
+    init(allocator, buffer.data, sizeof(buffer.data));
+    REQUIRE(allocator.size == sizeof(buffer.data));
     REQUIRE(allocator.top == 8);
     REQUIRE(allocator.last == 0);
 
-    void* first = BX_ALLOC(&allocator, 16);
+    // Non-aligned allocation I.
+    void* first = BX_ALLOC(&allocator, 13);
     REQUIRE(first != nullptr);
     REQUIRE(allocator.owns(first));
-    REQUIRE(allocator.size == sizeof(buffer));
-    REQUIRE(allocator.top == 32);
+    REQUIRE(allocator.size == sizeof(buffer.data));
+    REQUIRE(allocator.top == 29);
     REQUIRE(allocator.last == 8);
 
-    void* second = BX_ALLOC(&allocator, 8);
+    // Aligned allocation.
+    void* second = BX_ALIGNED_ALLOC(&allocator, 8, 16);
     REQUIRE(second != nullptr);
     REQUIRE(allocator.owns(second));
-    REQUIRE(allocator.top == 48);
-    REQUIRE(allocator.last == 32);
+    REQUIRE(allocator.top == 56);
+    REQUIRE(allocator.last == 40);
 
+    // Failed allocation (out of space).
     void* third = BX_ALLOC(&allocator, 128);
     REQUIRE(third == nullptr);
     REQUIRE(!allocator.owns(third));
-    REQUIRE(allocator.top == 48);
-    REQUIRE(allocator.last == 32);
+    REQUIRE(allocator.top == 56);
+    REQUIRE(allocator.last == 40);
 
-    BX_FREE(&allocator, third);
-    REQUIRE(allocator.top == 48);
-    REQUIRE(allocator.last == 32);
+    // No-op freeing of a `NULL` pointer.
+    BX_FREE(&allocator, nullptr);
+    REQUIRE(allocator.top == 56);
+    REQUIRE(allocator.last == 40);
 
-    void* second_realloced = BX_REALLOC(&allocator, second, 16);
+    // In-place reallocation.
+    void* second_realloced = BX_ALIGNED_REALLOC(&allocator, second, 16, 16);
     REQUIRE(second == second_realloced);
     REQUIRE(allocator.owns(second_realloced));
-    REQUIRE(allocator.top == 56);
-    REQUIRE(allocator.last == 32);
+    REQUIRE(allocator.top == 64);
+    REQUIRE(allocator.last == 40);
 
+    // Reallocation "anew".
     void* first_realloced = BX_REALLOC(&allocator, first, 8);
     REQUIRE(first != first_realloced);
     REQUIRE(allocator.owns(first_realloced));
-    REQUIRE(allocator.top == 72);
-    REQUIRE(allocator.last == 56);
+    REQUIRE(allocator.top == 80);
+    REQUIRE(allocator.last == 64);
 
+    // Freeing the middle block.
     BX_FREE(&allocator, second_realloced);
-    REQUIRE(allocator.top == 72);
-    REQUIRE(allocator.last == 56);
+    REQUIRE(allocator.top == 80);
+    REQUIRE(allocator.last == 64);
 
+    // Freeing last block (and also the last valid one).
     BX_FREE(&allocator, first_realloced);
     REQUIRE(allocator.top == 8);
     REQUIRE(allocator.last == 0);
