7c71442e4426e8165787c29c4627b23053d060a9
Jakub Fiser <fiser87@gmail.com>
2022-05-09 14:03:15 +0100

    Updated font atlas example.

--- src/test/font_atlas.c
+++ src/test/font_atlas.c
@@ -5,6 +5,9 @@
 #define ATLAS_ID 1
 
 #define TEXT_ID  1
+#define AXES_ID  32
+
+#include "fira_code_regular.h"
 
 static void setup(void)
 {
@@ -13,31 +16,46 @@ static void setup(void)
     clear_color(0x333333ff);
     clear_depth(1.0f);
 
-    create_font(FONT_ID, load_bytes("FiraCode-Regular.ttf", 0));
+    create_font(FONT_ID, fira_code_regular);
 
     begin_atlas(ATLAS_ID, ATLAS_H_OVERSAMPLE_2X | ATLAS_ALLOW_UPDATE, FONT_ID, 12.0f * dpi());
     glyph_range(0x20, 0x7e);
     end_atlas();
 
-    begin_text(TEXT_ID, ATLAS_ID, TEXT_H_ALIGN_CENTER | TEXT_V_ALIGN_MIDDLE);
+    const int h_align[] =
     {
-        color(0xffffffff);
-        text
-        (
-            "Put in in a deck for our standup today lose client to 10:00\n"
-            "meeting big picture, nor screw the pooch move the needle, so\n"
-            "enough to wash your face for we need to get all stakeholders up\n"
-            "to speed and in the right place. Are we in agreeance\n"
-            "incentivization so blue money, but regroup yet good optics\n"
-            "anti-pattern. Increase the pipelines. The last person we talked\n"
-            "to said this would be ready single wringable neck or usabiltiy.\n"
-            "Our competitors are jumping the shark. Re-inventing the wheel\n"
-            "can you slack it to me? Innovation is hot right now optics but\n"
-            "due diligence quantity.",
-            0
-        );
+        TEXT_H_ALIGN_LEFT,
+        TEXT_H_ALIGN_CENTER,
+        TEXT_H_ALIGN_RIGHT,
+    };
+
+    const int v_align[] =
+    {
+        TEXT_V_ALIGN_BASELINE,
+        TEXT_V_ALIGN_MIDDLE,
+        TEXT_V_ALIGN_CAP_HEIGHT,
+    };
+
+    for (int v = 0, i = 0; v < 3; v++     )
+    for (int h = 0       ; h < 3; h++, i++)
+    {
+        begin_text(TEXT_ID + i, ATLAS_ID, h_align[h] | v_align[v]);
+        {
+            color(0xffffffff);
+            text
+            (
+                "Put in on a deck for our standup\n"
+                "today lose client to 10:00 meeting\n"
+                "big picture, nor screw the pooch\n"
+                "move the needle, so enough to wash\n"
+                "your face for we need to get all\n"
+                "stakeholders up to speed and in\n"
+                "the right place.",
+                0
+            );
+        }
+        end_text();
     }
-    end_text();
 }
 
 static void draw(void)
@@ -49,22 +67,27 @@ static void draw(void)
 
     identity();
     ortho(0.0f, pixel_width(), pixel_height(), 0.0f, 1.0f, -1.0f);
-    // ortho(0.0f, pixel_width(), 0.0f, pixel_height(), 1.0f, -1.0f);
     projection();
 
     identity();
-    begin_mesh(2, MESH_TRANSIENT | PRIMITIVE_LINES | VERTEX_COLOR);
-    color(0x000000ff);
-    vertex(pixel_width() * 0.5f, -pixel_height(), 0.0f);
-    vertex(pixel_width() * 0.5f,  pixel_height(), 0.0f);
-    vertex(-pixel_width(), pixel_height() * 0.5f, 0.0f);
-    vertex( pixel_width(), pixel_height() * 0.5f, 0.0f);
+    begin_mesh(AXES_ID, MESH_TRANSIENT | PRIMITIVE_LINES | VERTEX_COLOR);
+    {
+        color (0xff0000ff);
+        vertex(pixel_width() * 0.5f, -pixel_height(), 0.0f);
+        vertex(pixel_width() * 0.5f,  pixel_height(), 0.0f);
+
+        color (0x00ff00ff);
+        vertex(-pixel_width(), pixel_height() * 0.5f, 0.0f);
+        vertex( pixel_width(), pixel_height() * 0.5f, 0.0f);
+    }
     end_mesh();
-    mesh(2);
+    mesh(AXES_ID);
 
     identity();
     translate(pixel_width() * 0.5f, pixel_height() * 0.5f, 0.0f);
-    mesh(TEXT_ID);
+
+    const int offset = (int)(elapsed() * 0.5) % 9;
+    mesh(TEXT_ID + offset);
 }
 
 MNM_MAIN(0, setup, draw, 0);
