aa6c3d3f755da15c76c6634343360bb43c1dad5f
Jakub Fiser <fiser87@gmail.com>
2022-05-19 09:44:12 +0100

    Added editor callback shells.

--- src/lib/rwr/mnm_rwr_editor.cpp
+++ src/lib/rwr/mnm_rwr_editor.cpp
@@ -1,8 +1,62 @@
 #include "mnm_rwr_lib.cpp"
 
-int main(int, char**)
+namespace mnm
+{
+
+namespace rwr
+{
+
+namespace ed
+{
+
+namespace
+{
+
+// -----------------------------------------------------------------------------
+// EDITOR CALLBACKS
+// -----------------------------------------------------------------------------
+
+void init(void)
 {
     // ...
+}
 
-    return 0;
+void setup(void)
+{
+    // ...
+}
+
+void draw(void)
+{
+    // ...
+}
+
+void cleanup(void)
+{
+    // ...
+}
+
+} // unnamed namespace
+
+} // namespace ed
+
+} // namespace rwr
+
+} // namespace mnm
+
+
+// -----------------------------------------------------------------------------
+// MAIN EDITOR ENTRY
+// -----------------------------------------------------------------------------
+
+int main(int, char**)
+{
+    // TODO : Argument processing (file to open, etc.).
+
+    return mnm_run(
+        ed::init,
+        ed::setup,
+        ed::draw,
+        ed::cleanup
+    );
 }
